a20fd0353fa1fb7703a4ab36236c674620fd38a7
Shosetsu <Shosetsu.rr@outlook.com>
2023-04-29 12:09:31 +0800

    change header works

--- src/app/page/header/header.component.ts
+++ src/app/page/header/header.component.ts
@@ -3,6 +3,7 @@ import { ActivationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs';
 import { Constants } from 'src/app/constants/cosntants';
 import { DataService } from 'src/app/core/data.service';
+import { SystemService } from 'src/app/core/system.service';
 import { SystemData } from 'src/app/interface/system.storage';
 
 @Component({
@@ -11,11 +12,15 @@ import { SystemData } from 'src/app/interface/system.storage';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  layers: { name: string; url: string; order: number }[] = [];
+  layers: TitlePoint[] = [];
 
   currentLanguage = '';
 
-  constructor(private router: Router, private data: DataService) {}
+  constructor(
+    private router: Router,
+    private data: DataService,
+    private system: SystemService
+  ) {}
 
   ngOnInit(): void {
     this.currentLanguage =
@@ -29,18 +34,30 @@ export class HeaderComponent implements OnInit {
         )
       )
       .subscribe((event) => {
-        const order = event.snapshot.data['order'] ?? 0;
-        const names = event.snapshot.title?.split('｜') ?? [];
-
-        this.layers = [
-          ...this.layers.filter((layer) => layer.order < order),
-          {
-            name: names[1] ?? names[0],
-            url: event.snapshot.url[0]?.path ?? '',
-            order: order,
-          },
-        ];
+        this.layers = this.getLayers(event);
       });
+  }
+
+  private getLayers(event: ActivationEnd): TitlePoint[] {
+    const url = event.snapshot.url[0]?.path ?? '';
+    const findTitle = (
+      url: string,
+      base: TitlePoint,
+      tree: TitlePoint[]
+    ): TitlePoint[] | undefined => {
+      const nowLayers = [
+        ...tree,
+        {
+          ...base,
+          name: this.system.langText[base.name],
+        },
+      ];
+      if (base.url === url) return nowLayers;
+      let tested: TitlePoint[] | undefined;
+      base.children?.some((tp) => (tested = findTitle(url, tp, nowLayers)));
+      return tested;
+    };
+    return findTitle(url, titles, []) ?? [];
   }
 
   changeLanguage() {
@@ -50,3 +67,19 @@ export class HeaderComponent implements OnInit {
     location.reload();
   }
 }
+
+export type TitlePoint = {
+  order: number;
+  name: string;
+  url: string;
+  children?: TitlePoint[];
+};
+export const titles: TitlePoint = {
+  order: 0,
+  name: 'MAIN_TITLE',
+  url: '',
+  children: [
+    { order: 1, name: 'REOLL_TODAY_TITLE', url: 'today' },
+    { order: 1, name: 'RESIN_MEMO_TITLE', url: 'resin' },
+  ],
+};
