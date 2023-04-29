f52a33ff241c7460616b7c4fe8451e3d80f328c1
Shosetsu <Shosetsu.rr@outlook.com>
2023-04-29 12:09:08 +0800

    test class removed

--- src/app/directive/text.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TextDirective } from './text.directive';
-
-describe('TextDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TextDirective();
-    expect(directive).toBeTruthy();
-  });
-});
